6e5251a426e88f1a9f958374d2eb716056ba9cae
christides11 <christides11@hotmail.com>
2019-10-22 20:35:24 -0400

    Start of Dynamic AABB Tree implementation, Reorganized project for other examples later.

--- Assets/2DFPhysics/Core/Collision/AABB.cs
+++ Assets/2DFPhysics/Core/Collision/AABB.cs
@@ -6,5 +6,19 @@ namespace TDFP.Core{
     {
         public FixVec2 min;
         public FixVec2 max;
+
+        public static AABB Union(AABB a, AABB b)
+        {
+            AABB C;
+            C.min = new FixVec2(FixMath.Min(a.min.X, b.min.X), FixMath.Min(a.min.Y, b.min.Y));
+            C.max = new FixVec2(FixMath.Min(a.max.X, b.max.X), FixMath.Min(a.max.Y, b.max.Y));
+            return C;
+        }
+
+        public static Fix Area(AABB a)
+        {
+            FixVec2 d = a.max - a.min;
+            return d.X * d.Y;
+        }
     }
 }
---
+++ Assets/2DFPhysics/Core/Collision/DynamicTree.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using FixedPointy;
+using System.Collections.Generic;
+using UnityEngine.Profiling;
+using System;
+
+//https://www.toptal.com/game/video-game-physics-part-ii-collision-detection-for-solid-objects
+namespace TDFP.Core
+{
+    public class DynamicTree
+    {
+
+    }
+}
